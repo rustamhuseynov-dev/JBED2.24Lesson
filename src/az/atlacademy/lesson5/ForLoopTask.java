01481b02cb5867188c92215c5b015b786067912a
rustemcoding571 <rustem.huseynov.2016@gmail.com>
2024-06-09 22:07:30 +0400

    the case was successful

--- src/az/atlacademy/lesson5/ForLoopTask.java
+++ src/az/atlacademy/lesson5/ForLoopTask.java
@@ -11,6 +11,9 @@ public class ForLoopTask {
         //question5();
         //question7(0,0);
         //question8();
+        //question9();
+        //question10();
+        question11();
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.print("Enter an integer: ");
 //        int number = scanner.nextInt();
@@ -18,6 +21,58 @@ public class ForLoopTask {
 //        System.out.println("Reversed number: " + reversedNumber);
 //        scanner.close();
     }
+
+    private static void question11() {
+
+    }
+
+    private static void question10() {
+        Scanner sc = new Scanner(System.in);
+        char choice;
+        do {
+            System.out.println("ilk sayi girin : ");
+            int num = sc.nextInt();
+            System.out.println("ikinci sayi girin : ");
+            int num1 = sc.nextInt();
+
+            int total = num + num1;
+            System.out.println("Toplam : " + total);
+            System.out.println("yeniden istifade etmek isteyirsinizmi? (isteyirsinizse 'Y',istemirsizse 'N' basin");
+            choice = sc.next().charAt(0);
+        } while (choice == 'Y' || choice == 'y');
+
+        sc.close();
+    }
+
+    private static void question9() {
+        Scanner scanner = new Scanner(System.in);
+
+        // İki sayıyı oku
+        System.out.println("İlk sayıyı giriniz:");
+        int a = scanner.nextInt();
+        System.out.println("İkinci sayıyı giriniz:");
+        int b = scanner.nextInt();
+
+        // Euclidean algoritmasını kullanarak EBOB'u hesapla
+        int hcf = calculateHCF(a, b);
+
+        // Sonucu yazdır
+        System.out.println(a + " ve " + b + " sayılarının EBOB'u: " + hcf);
+
+        // Scanner'ı kapat
+        scanner.close();
+    }
+
+    // Euclidean algoritmasını kullanarak EBOB'u hesaplayan metod
+    public static int calculateHCF(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
 
     public static int reverseNumber(int number) {
         int reversedNumber = 0;
