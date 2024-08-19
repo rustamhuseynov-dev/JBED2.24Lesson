c028960a5e2ac753d1a58a0db917174415b004fe
rustemcoding571 <rustem.huseynov.2016@gmail.com>
2024-08-19 21:34:50 +0400

    declarative programming

--- src/az/atlacademy/lesson35/FunctionalProgramming/FunctionExample.java
+++ src/az/atlacademy/lesson35/FunctionalProgramming/FunctionExample.java
@@ -1,2 +1,18 @@
-package az.atlacademy.lesson35.FunctionalProgramming;public class FunctionExample {
+package az.atlacademy.lesson35.FunctionalProgramming;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class FunctionExample {
+    public static void main(String[] args) {
+
+        Function<String, Integer> function = s -> s.length();
+
+        function = f -> f.length();
+        int result = function.apply("Salam");
+        System.out.println(result);
+
+        Predicate<String> predicate = s -> false;
+
+    }
 }
--- src/az/atlacademy/lesson35/FunctionalProgramming/LambdaExpression.java
+++ src/az/atlacademy/lesson35/FunctionalProgramming/LambdaExpression.java
@@ -1,2 +1,17 @@
-package az.atlacademy.lesson35.FunctionalProgramming;public class LambdaExpression {
+package az.atlacademy.lesson35.FunctionalProgramming;
+
+import java.util.List;
+import java.util.Random;
+
+
+public class LambdaExpression {
+    public static void main(String[] args) {
+        List<String> list= new Random()
+                .ints(50,0,100)
+                .filter(n -> n % 2 !=0)
+                .boxed()
+                .map(String::valueOf)
+                .toList();
+        System.out.println(list);
+    }
 }
